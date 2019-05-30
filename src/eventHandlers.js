45050db40df65194c41a352a42e42777df79848e
Alejandro Aguilar <aaguilar2@hou-us05186-mbp13.attlocal.net>
2019-05-29 20:25:47 -0500

    after lecture

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -1,27 +1,27 @@
-function handleSearchInput(event, allPokemonData, pokemonContainer) {
-  const filteredPokes = allPokemonData.filter(pokeObj => {
-    return pokeObj.name.includes(event.target.value.toLowerCase())
-  })
-  const filteredPokeHTML = renderAllPokemon(filteredPokes)
-  pokemonContainer.innerHTML = filteredPokeHTML ? filteredPokeHTML : `<p><center>There are no Pokémon here</center></p>`
-}
+// function handleSearchInput(event, allPokemonData, pokemonContainer) {
+//   const filteredPokes = allPokemonData.filter(pokeObj => {
+//     return pokeObj.name.includes(event.target.value.toLowerCase())
+//   })
+//   const filteredPokeHTML = renderAllPokemon(filteredPokes)
+//   pokemonContainer.innerHTML = filteredPokeHTML ? filteredPokeHTML : `<p><center>There are no Pokémon here</center></p>`
+// }
 
 
-function handleImgClick(event, allPokemonData) {
-  if (event.target.dataset.action === 'flip') {
-    const clickedPokemon = allPokemonData.find((pokemonObject) => pokemonObject.id == event.target.dataset.id)
-    event.target.src = (event.target.src === clickedPokemon.sprites.front ? clickedPokemon.sprites.back : clickedPokemon.sprites.front)
-  }
-}
+// function handleImgClick(event, allPokemonData) {
+//   if (event.target.dataset.action === 'flip') {
+//     const clickedPokemon = allPokemonData.find((pokemonObject) => pokemonObject.id == event.target.dataset.id)
+//     event.target.src = (event.target.src === clickedPokemon.sprites.front ? clickedPokemon.sprites.back : clickedPokemon.sprites.front)
+//   }
+// }
 
 /************************* Helper Fns for Producing HTML **********************/
-function renderAllPokemon(pokemonArray) {
-  return pokemonArray.map(renderSinglePokemon).join('')
-}
+// function renderAllPokemon(pokemonArray) {
+//   return pokemonArray.map(renderSinglePokemon).join('')
+// }
 
 function renderSinglePokemon(pokemon) {
   return (`
-  <div class="pokemon-card">
+  <div class="pokemon-card" id="pokemon${pokemon.id}">
     <div class="pokemon-frame">
       <h1 class="center-text">${pokemon.name}</h1>
       <div class="pokemon-image">
--- src/index.js
+++ src/index.js
@@ -3,15 +3,39 @@ document.addEventListener('DOMContentLoaded', () => {
   const pokemonContainer = document.querySelector('#pokemon-container')
   const pokemonSearchInput = document.querySelector('#pokemon-search-form')
 
-  fetch('http://localhost:3000/pokemon'/*, { method: 'GET' }*/)
-    .then((responseObject) => responseObject.json())
-    .then((pokeJSONData) => {
-      allPokemonData = pokeJSONData
-      pokemonContainer.innerHTML = renderAllPokemon(allPokemonData)
+  // fetch('http://localhost:3000/pokemon'/*, { method: 'GET' }*/)
+  //   .then((responseObject) => responseObject.json())
+  //   .then((pokeJSONData) => {
+  //     allPokemonData = pokeJSONData
+  //     pokemonContainer.innerHTML = renderAllPokemon(allPokemonData)
+  //   })
+
+  //   pokemonSearchInput.addEventListener('input', (event) => handleSearchInput(event, allPokemonData, pokemonContainer))
+
+  //   pokemonContainer.addEventListener('click', (event) => handleImgClick(event, allPokemonData))
+
+  fetch("http://localhost:3000/pokemon")
+    .then(resp => resp.json())
+    .then(pokeData => {
+      const allPoke = pokeData.map(pokemon => renderSinglePokemon(pokemon)).join("")
+      pokemonContainer.innerHTML += allPoke
     })
 
-    pokemonSearchInput.addEventListener('input', (event) => handleSearchInput(event, allPokemonData, pokemonContainer))
 
-    pokemonContainer.addEventListener('click', (event) => handleImgClick(event, allPokemonData))
+  // const pokemon = renderSinglePokemon(onePokemon)
+  pokemonContainer.addEventListener("click", function(e){
+    if (e.target.className === "pokemon-button"){
+      const pokemonTBD = e.target.parentElement.parentElement
+      const pokeId = pokemonTBD.id.split("pokemon")[1]
+      fetch(`http://localhost:3000/pokemon/${pokeId}`,{
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+      }
+      }).then(() => pokemonTBD.remove())
+
+      // pokemonTBD.remove()
+    }
+  })
 
 })
